25197d01e286058d898164e6a50684b8ac96ef43
ionlizarazu <ionlizarazu@gmail.com>
2015-03-06 09:15:47 +0100

    Ejercicio finalizado

--- Ejercicio JS Prototype/prototype.js
+++ Ejercicio JS Prototype/prototype.js
@@ -1,27 +1,42 @@
 function Persona (nombre, edad,genero) {
-    this.nombre = nombre || "";
-    this.edad = edad || "";
-    this.genero = genero || "";
-    this.obtDetalles(alert(this.nombre));
+    this.nombre = nombre;
+    this.edad = edad;
+    this.genero = genero;
+    this.obtDetalles = function(){
+        console.log("Nombre: ",this.nombre," Edad: ", this.edad," Género: ", this.genero);
+    };
 }
  
-function Estudiante (nombre, curso, grupo) {
+function Profesor (nombre,edad, genero,  asignatura, nivel) {
   this.base = Persona;
-  this.base(nombre, curso, grupo);
-  this.registrar();
-}
-Estudiante.prototype = new Persona;
- 
-function Profesor (nombre, asignatura, nivel) {
-  this.base = Persona;
-  this.base(nombre, asignatura, nivel);
-  this.asignar();
+  this.base(nombre, edad, genero);
+    this.asignatura = asignatura;
+    this.nivel = nivel;
+  this.asignar = function(){
+    console.log("Nombre: ",this.nombre," Edad: ", this.edad," Género: ", this.genero, " Asignatura: ",this.asignatura," Nivel: ", this.nivel);
+  };
 }
 Profesor.prototype = new Persona;
+ 
+function Estudiante (nombre,edad,genero,curso, grupo) {
+  this.base = Persona;
+  this.base(nombre, edad, genero);
+    this.curso = curso;
+    this.grupo = grupo;
+  this.registrar = function(){
+    console.log("Nombre: ",this.nombre," Edad: ", this.edad," Género: ", this.genero, " Curso: ", this.curso, " Grupo: ", this.grupo);
+  };
+}
 
-    
-var estu = new Estudiante("ion",1,"dam");
-   // alert(estu.__proto__ == Estudiante.prototype);
-var pers = new Persona("ion","11","m");
-    //alert(pers.__proto__ == Persona.prototype);
-    pers.obtDetalles();
+Estudiante.prototype = new Persona;
+
+var per1 = new Persona("ion",22,"M");
+per1.obtDetalles();
+
+var est1 = new Estudiante("ion",22,"M", 2, 1);
+est1.obtDetalles();
+est1.registrar();
+
+var pro1 = new Profesor("ion",22,"M", "Historia", 1);
+pro1.obtDetalles();
+pro1.asignar();
