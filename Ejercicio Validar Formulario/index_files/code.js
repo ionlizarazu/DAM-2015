849678c413425c853758749e4892292a8016611b
ionlizarazu <ionlizarazu@gmail.com>
2015-03-10 10:44:26 +0100

    Ejercicio Validar Formulario validaciones varias

--- Ejercicio Validar Formulario/index_files/code.js
+++ Ejercicio Validar Formulario/index_files/code.js
@@ -1,7 +1,26 @@
 var nomE = document.getElementById("registro_nombre");
+function validarNombre(){
+    if(/^\s*$/.test(nomE.value)||!/[a-zA-Z]+/.test(nomE.value)){
+       console.log("Campo de Nombre obligatorio");
+    }
+}
+var apeE = document.getElementById("registro_apellido");
+function validarApellidos(){
+    if(/^\s*$/.test(apeE.value)||!/[a-zA-Z]+/.test(apeE.value)){
+       console.log("Campo de Apellidos obligatorio");
+    }
+}
 
-if(nomE.onclick==true){
-
-    console.log("dfsdfsd");
-
+APP.validarEmail = function validarEmail(){
+    var emailRegExp = /[a-zA-Z_\.]+@[a-zA-Z]+\.[a-zA-Z]{2,3}/;
+    var emailValue = document.getElementById("registro_email").value;
+    if(!emailRegExp.test(emailValue)){
+        console.log("Email incorrecto");
+    }
+}
+APP.validarPass = function validarPass(){
+    var passValue = document.getElementById("registro_password").value;
+    if(/^\s*$/.test(passValue)){
+        console.log("Password incorrecto");
+    }
 }
--- Ejercicio Validar Formulario/index_files/validador.js
+++ Ejercicio Validar Formulario/index_files/validador.js
@@ -1,4 +1,13 @@
-valor = document.getElementById("campo").value;
-if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
-  return false;
+"use strict";
+
+var APP = APP|| {};
+
+window.addEventListener("load", setListeners, false);
+
+function setListeners(){
+    
+    document.getElementById("registro_email").addEventListener("blur",APP.validarEmail, false);
+    
+    document.getElementById("registro_password").addEventListener("blur",APP.validarPass, false);
+    
 }
